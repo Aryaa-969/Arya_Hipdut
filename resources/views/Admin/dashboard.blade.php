c6c3f220aa1e22d49f72e4c9513e046da6cc2442
Aryaa-969 <arya24si@mahasiswa.pcr.ac.id>
2025-10-13 08:19:28 +0700

    Form Volt

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -138,7 +138,7 @@
 					<div class="multi-level collapse " role="list" id="submenu-app" aria-expanded="false">
 						<ul class="flex-column nav">
 							<li class="nav-item ">
-								<a class="nav-link" href="../../pages/tables/bootstrap-tables.html">
+								<a class="nav-link" href="assets-admin/pages/tables/bootstrap-tables.html">
 									<span class="sidebar-text">Bootstrap Tables</span>
 								</a>
 							</li>
